3aa877c0b8b989fd0587e5aeae13f7c49e87f8c2
Danvy Vu <d.v.vu@student.tue.nl>
2023-05-15 12:32:15 +0200

    Revert "String objects are not accepted anymore in sckOut function"
    This reverts commit 4c3c9c71f97f4aee6ec8c0b1b600e1472be7eb61.

--- sam/src/Commands.cpp
+++ sam/src/Commands.cpp
@@ -263,8 +263,7 @@ void sensorConfig_com(SckBase* base, String parameters)
 			uint8_t newEveryNint = intervalInt / base->config.readInterval;
 			if (newEveryNint < 1) newEveryNint = 1;
 
-			snprintf(base->outBuff, sizeof(base->outBuff), "The sensor read interval is calculated as a multiple of general read interval (%u)", base->config.readInterval);
-			base->sckOut();
+			base->sckOut("The sensor read interval is calculated as a multiple of general read interval (" + String(base->config.readInterval) + ")");
 			if (newEveryNint < 255) {
 				if (groupToChange_size > 0) {
 					// Just for PM/PN change all the sensors in the same group
